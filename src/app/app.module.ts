098f0227a019742286be4e28f3bc6b7d10bb0d40
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2021-03-19 13:47:57 +0100

    Pushed final version for presentation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {HomePageComponent} from './pages/home-page/home-page.component';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { GraphComponent } from './components/graph/graph.component';
+
+const routes: Routes = [
+  {path: 'home', component: HomePageComponent},
+  {path: '', component: HomePageComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    GraphComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    RouterModule.forRoot(routes),
+    NgxChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
